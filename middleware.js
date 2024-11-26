a2e8231c00391fa66ebf1f3b58725a924e79427a
chetan360 <chetansmankar360@gmail.com>
2024-11-26 17:59:15 +0530

    resolve login flaw for adding, edditing listing (redirection)

--- middleware.js
+++ middleware.js
@@ -1,7 +1,17 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.flash("error", "You must be logged in to create listing!");
+    // store the redirect url
+    req.session.redirectUrl = req.originalUrl;
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    // preserve the redirect url in locals
+    res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
 };
